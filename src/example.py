7927be05f8adfcf6024ad37024b7c5f1950d7657
Commander07 <olledubbelw@gmail.com>
2021-06-06 17:42:33 +0200

    Fixed styling

--- src/example.py
+++ src/example.py
@@ -1,8 +1,11 @@
 from .hydrogen import Mod, EventHandler, events
 META = Mod("Example Mod", "Commander", "Example hydrogen mod", "hydrogen:example")
+
+
 class example(META):
   def __init__(self):
     super().__init__()
+
   @EventHandler(events.OnEnable)
   def OnEnable(e):
     print(e)
--- src/hydrogen.py
+++ src/hydrogen.py
@@ -4,27 +4,38 @@ author: https://github.com/commander07
 description: Hydrogen Mod Loader (HML) is a modding framework for python games. That has features such as events, override and more!
 """
 from dataclasses import dataclass
-class GLOBAL:pass
+class GLOBAL: pass
+
+
 class events:
   @dataclass
-  class Event:name: str
+  class Event: name: str
   OnEnable = Event("OnEnable")
   OnDisable = Event("OnDisable")
+
+
 class EventHandler:
   def __init__(self, event: events.Event):
     self.event = event
+
   def __call__(self, handle):
     setattr(GLOBAL, "example", {})
     getattr(GLOBAL, "example")[self.event.name] = handle
-    
+
     return handle
+
+
 def load_mod(self):
   try:
     getattr(GLOBAL, "example")["OnEnable"](events.OnEnable)
   except KeyError:
     pass
+
+
 def init_mod(self):
   self.load()
+
+
 def Mod(name: str, author: str, description: str, id: str):
     return type("mod", (object, ), {
       "__init__": init_mod,
